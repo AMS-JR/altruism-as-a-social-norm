005bbe646689becf735bb972792c6a2db39a1834
AMS-JR <asj.sarjo@gmail.com>
2022-07-29 15:59:07 +0200

    some changes

--- main.py
+++ main.py
@@ -29,9 +29,8 @@ def play(N, episodes, time_steps, model, endowment, h, l, transient_time_steps, 
     """
     average_aspirations = []
     average_donations = []
-    stationary_counter = 0
-    stationary_state = False
     free_rider = random.randint(0, N)
+    # print("free_rider: ",free_rider)
     for episode in range(episodes):
         print(f"episode: {episode}")
         # Aspirations and Donations Space -> can i have just 1D-array of N for each(A and D)? test if results are same
@@ -40,76 +39,83 @@ def play(N, episodes, time_steps, model, endowment, h, l, transient_time_steps, 
         # Initial aspirations and donations
         A[0] = np.random.uniform(0, 1, N)
         D[0] = endowment - A[0]
+        stationary_counter = 0
+        stationary_state = False
         # print(A)
         # print(D)
-        for t in range(time_steps):
-            # pairs_of_individuals[[Dictator,Recipient]]
-            pairs_of_individuals = np.arange(N).reshape(int(N/2), 2)  # 1000 individuals
-            np.random.shuffle(pairs_of_individuals.flat)
+        while not stationary_state:
+            for ts in range(time_steps):
+                t = ts + stationary_counter * time_steps
+                # pairs_of_individuals[[Dictator,Recipient]]
+                pairs_of_individuals = np.arange(N).reshape(int(N/2), 2)  # 1000 individuals
+                np.random.shuffle(pairs_of_individuals.flat)
 
-            for i in range(len(pairs_of_individuals)):
-                if model == "stochastic":
-                    # for testing, put noise=standard_deviation as is in the article i believe
-                    noise = np.random.normal(0, standard_deviation)
-                    D[t][pairs_of_individuals[i][0]] = (1 + noise) * D[t][pairs_of_individuals[i][0]]
-                    D[t][pairs_of_individuals[i][1]] = (1 + noise) * D[t][pairs_of_individuals[i][1]]
-                elif model == "envious":
-                    prob_estimate = np.random.uniform(0.0, high=1.0, size=None)
-                    if prob_estimate <= 0.05:
-                        # print(f"prob_estimate less ~ Donation: {D[t + 1][pairs_of_individuals[i][1]]}")
-                        D[t][pairs_of_individuals[i][0]] = min(D[t][pairs_of_individuals[i][0]], 0.5)
-                        D[t][pairs_of_individuals[i][1]] = min(D[t][pairs_of_individuals[i][1]], 0.5)
-                elif model == "free-riders":
-                    if pairs_of_individuals[i][0] == free_rider:    # Dictator is a free-rider
-                        D[t][pairs_of_individuals[i][0]] = 0
-                        # print(f"free-riders ~ Dictator: {D[t][pairs_of_individuals[i][0]]}")
-                    if pairs_of_individuals[i][1] == free_rider:    # Recipient is a free-rider
-                        D[t][pairs_of_individuals[i][1]] = 0
-                        # print(f"free-riders ~ Recipient: {D[t+1][pairs_of_individuals[i][1]]}")
-                else:
-                    pass
-                # calculate stimuli for recipient
-                if endowment != A[t][pairs_of_individuals[i][1]]:
-                    s = (D[t][pairs_of_individuals[i][0]] - A[t][pairs_of_individuals[i][1]])/(endowment - A[t][pairs_of_individuals[i][1]])
-                    if s < -1:
-                        s = -1
-                    if s > 1:
-                        s = 1
-                else:
-                    s = 0
-                # update A[i+1]
-                if s >= 0:
-                    change_in_aspiration = ((endowment - A[t][pairs_of_individuals[i][1]]) * l * s)
-                    A[t+1][pairs_of_individuals[i][1]] = A[t][pairs_of_individuals[i][1]] + change_in_aspiration
-                else:
-                    change_in_aspiration = (A[t][pairs_of_individuals[i][1]] * l * s)
-                    A[t+1][pairs_of_individuals[i][1]] = A[t][pairs_of_individuals[i][1]] + change_in_aspiration
-                # update D[i+1]
-                payoff = (h * D[t][pairs_of_individuals[i][0]])
-                D[t+1][pairs_of_individuals[i][1]] = ((1 - h) * D[t][pairs_of_individuals[i][1]]) + payoff
-                D[t+1][pairs_of_individuals[i][1]] = max(0, min(D[t+1][pairs_of_individuals[i][1]], (endowment - A[t+1][pairs_of_individuals[i][1]])))
-                # ifelifelse
-                # aspirations and donations of dictator individuals are unchanged at the beginning of next time step
-                A[t+1][pairs_of_individuals[i][0]] = A[t][pairs_of_individuals[i][0]]
-                D[t+1][pairs_of_individuals[i][0]] = D[t][pairs_of_individuals[i][0]]
-        stationary_counter += 1
-        # Check for Stationary state after each episode to end iterations
-        if stationary_counter*time_steps >= transient_time_steps:
-            # play with the slice and write something better
-            print(f"Aspirations: {A}")
-            print(f"Donations: {D}")
-            increment = 4
-            while increment < stationary_counter:
-                mean_donations_forward = np.mean(D[int(increment*(time_steps/stationary_counter)):], axis=0)
-                slope = np.max(mean_donations_forward) - np.min(mean_donations_forward)
-                print("slope: ", slope)
-                stationary_state = slope < 10**(-4)
-                increment += 1
-                if stationary_state:
-                    break
-
-        if stationary_state:
-            break
+                for i in range(len(pairs_of_individuals)):
+                    if model == "stochastic":
+                        # for testing, put noise=standard_deviation as is in the article i believe
+                        noise = np.random.normal(0, standard_deviation)
+                        D[t][pairs_of_individuals[i][0]] = (1 + noise) * D[t][pairs_of_individuals[i][0]]
+                        D[t][pairs_of_individuals[i][1]] = (1 + noise) * D[t][pairs_of_individuals[i][1]]
+                    if model == "envious":
+                        prob_estimate = np.random.uniform(0.0, high=1.0, size=None)
+                        if prob_estimate <= 0.05:
+                            # print(f"prob_estimate less ~ Donation: {D[t + 1][pairs_of_individuals[i][1]]}")
+                            D[t][pairs_of_individuals[i][0]] = min(D[t][pairs_of_individuals[i][0]], 0.5)
+                            D[t][pairs_of_individuals[i][1]] = min(D[t][pairs_of_individuals[i][1]], 0.5)
+                    if model == "free-riders":
+                        if pairs_of_individuals[i][0] == free_rider:    # Dictator is a free-rider
+                            D[t][pairs_of_individuals[i][0]] = 0
+                            # print(f"free-riders ~ Dictator: {D[t][pairs_of_individuals[i][0]]}")
+                        if pairs_of_individuals[i][1] == free_rider:    # Recipient is a free-rider
+                            D[t][pairs_of_individuals[i][1]] = 0
+                            # print(f"free-riders ~ Recipient: {D[t+1][pairs_of_individuals[i][1]]}")
+                    # else:
+                        # pass
+                    # calculate stimuli for recipient
+                    if endowment != A[t][pairs_of_individuals[i][1]]:
+                        s = (D[t][pairs_of_individuals[i][0]] - A[t][pairs_of_individuals[i][1]])/(endowment - A[t][pairs_of_individuals[i][1]])
+                        if s < -1:
+                            s = -1
+                        if s > 1:
+                            s = 1
+                    else:
+                        s = 0
+                    # update A[i+1]
+                    if s >= 0:
+                        change_in_aspiration = ((endowment - A[t][pairs_of_individuals[i][1]]) * l * s)
+                        A[t+1][pairs_of_individuals[i][1]] = A[t][pairs_of_individuals[i][1]] + change_in_aspiration
+                    else:
+                        change_in_aspiration = (A[t][pairs_of_individuals[i][1]] * l * s)
+                        A[t+1][pairs_of_individuals[i][1]] = A[t][pairs_of_individuals[i][1]] + change_in_aspiration
+                    # update D[i+1]
+                    payoff = (h * D[t][pairs_of_individuals[i][0]])
+                    D[t+1][pairs_of_individuals[i][1]] = ((1 - h) * D[t][pairs_of_individuals[i][1]]) + payoff
+                    D[t+1][pairs_of_individuals[i][1]] = max(0, min(D[t+1][pairs_of_individuals[i][1]], (endowment - A[t+1][pairs_of_individuals[i][1]])))
+                    # ifelifelse
+                    # aspirations and donations of dictator individuals are unchanged at the beginning of next time step
+                    A[t+1][pairs_of_individuals[i][0]] = A[t][pairs_of_individuals[i][0]]
+                    D[t+1][pairs_of_individuals[i][0]] = D[t][pairs_of_individuals[i][0]]
+            stationary_counter += 1
+            # Check for Stationary state after each episode to end iterations
+            if stationary_counter*time_steps >= transient_time_steps:
+                # play with the slice and write something better
+                # print(f"Aspirations: {A}")
+                # print(f"Donations: {D}")
+                increment = 5
+                while increment < stationary_counter:
+                    mean_donations_forward = np.mean(D[((stationary_counter - 1)*time_steps) + int(increment*(time_steps/stationary_counter)):], axis=0)
+                    slope = np.max(mean_donations_forward) - np.min(mean_donations_forward)
+                    print("slope: ", slope)
+                    stationary_state = slope < 10**(-4)
+                    increment += 1
+                    if stationary_state:
+                        print("Stable Donations: ", D)
+                        break
+            if not stationary_state:
+                A = np.resize(A, ((stationary_counter+1)*time_steps + 1, N))
+                D = np.resize(D, ((stationary_counter+1)*time_steps + 1, N))
+        # if stationary_state:
+        #     break
         # find averages
         average_aspirations.append(np.mean(A, axis=0))
         average_donations.append(np.mean(D, axis=0))
@@ -123,7 +129,7 @@ if __name__ == '__main__':
     ls = np.array([0.2, 0.4, 0.6, 0.8])
     hs = np.array([0.2, 0.4, 0.6, 0.8])
     N = 1000
-    episodes = 20
+    episodes = 50
     time_steps = 1000
     transient_time_steps = 10000
     model = "deterministic"
@@ -183,5 +189,6 @@ if __name__ == '__main__':
             rects1 = my_plotter(axs, x - width / 2, frequencies_aspirations[i], "Aspirations", (j, k))
             rects2 = my_plotter(axs, x + width / 2, frequencies_donations[i], "Donations", (j, k))
             i +=1
+
     plt.subplots_adjust(hspace=0.35)
     plt.show()
